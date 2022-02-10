2cf150505ba97498d48686393aa0225aedffbd94
Matt Haines <mhpahuta@gmail.com>
2022-02-10 11:13:48 -0800

    Add ghost movement functionality

--- app.js
+++ app.js
@@ -161,4 +161,36 @@ const ghosts = [
 ]
 
 // add ghosts to grid
-ghosts.forEach(ghost => squares[ghost.startIndex].classList.add(ghost.className))
+ghosts.forEach(ghost => {
+  squares[ghost.currentIndex].classList.add(ghost.className)
+  squares[ghost.currentIndex].classList.add('ghost')
+});
+
+// move ghosts around board
+ghosts.forEach(ghost => moveGhost(ghost))
+
+function moveGhost(ghost) {
+  console.log('moved ghost')
+  const directions = [-1, +1, -width, +width]
+  let direction = directions[Math.floor(Math.random() * directions.length)]
+  console.log(direction);
+
+  ghost.timerId = setInterval(function() {
+    
+    if (
+      !squares[ghost.currentIndex + direction].classList.contains('wall') &&
+      !squares[ghost.currentIndex + direction].classList.contains('ghost')) {
+        squares[ghost.currentIndex].classList.remove(ghost.className); // remove ghost
+        squares[ghost.currentIndex].classList.remove('ghost'); // remove ghost
+        ghost.currentIndex += direction; // add direction to current index
+
+        squares[ghost.currentIndex].classList.add(ghost.className)
+        squares[ghost.currentIndex].classList.add('ghost');
+    } else {
+      direction = directions[Math.floor(Math.random() * directions.length)]
+    }
+
+
+  }, ghost.speed)
+}
+
