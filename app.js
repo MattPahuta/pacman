381a8a67666d2d47d036864098f92c7a3a36852f
Matt Haines <mhpahuta@gmail.com>
2022-02-09 16:00:29 -0800

    Add ghosts constructor method, add to board, styles

--- app.js
+++ app.js
@@ -141,4 +141,24 @@ function pacDotEaten() {
   }
 }
 
-// make ghosts
+// make ghosts - 
+class Ghost {
+  constructor(className, startIndex, speed) {
+    this.className = className
+    this.startIndex = startIndex
+    this.speed = speed
+    this.currentIndex = startIndex
+    this.isScared = false
+    this.timerId = NaN
+  }
+}
+
+const ghosts = [
+  new Ghost('blinky', 348, 250),
+  new Ghost('pinky', 376, 400),
+  new Ghost('inky', 351, 300),
+  new Ghost('clyde', 379, 500)
+]
+
+// add ghosts to grid
+ghosts.forEach(ghost => squares[ghost.startIndex].classList.add(ghost.className))
